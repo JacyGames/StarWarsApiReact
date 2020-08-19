ab67bd6a63b0dadf009e10be0bd2c3c9d668cba1
JacyGames <bohdan.danultsiv@gmail.com>
2020-08-19 20:14:35 +0300

    Updates

---
+++ precache-manifest.19ac83fa9b02d75806f6c8fbb0f79178.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5034f229f01fdbb7a0bccbb0a6111cc",
+    "url": "/StarWarsApiReact/index.html"
+  },
+  {
+    "revision": "edf96f68f24f35751b5d",
+    "url": "/StarWarsApiReact/static/css/main.9283f8d8.chunk.css"
+  },
+  {
+    "revision": "0538035272354f868fae",
+    "url": "/StarWarsApiReact/static/js/2.9a1ed860.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/StarWarsApiReact/static/js/2.9a1ed860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf96f68f24f35751b5d",
+    "url": "/StarWarsApiReact/static/js/main.e081d6d8.chunk.js"
+  },
+  {
+    "revision": "e57df1b6262dda00b30e",
+    "url": "/StarWarsApiReact/static/js/runtime-main.f4fa8637.js"
+  },
+  {
+    "revision": "3cd88db77b031b5488d6d12f2fe9a5fd",
+    "url": "/StarWarsApiReact/static/media/planets-saturn-img.3cd88db7.jpg"
+  }
+]);
